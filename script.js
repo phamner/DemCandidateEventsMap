874eacc65593ab01787d2b26defece12d4cd9a07
Philip Hamner <philiphamner@gmail.com>
2019-11-26 00:49:12 -0800

    Add InfoWindow for each map marker.

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// window.fbAsyncInit = function() {
-// FB.init({
-//     appId            : 'your-app-id',
-//     autoLogAppEvents : true,
-//     xfbml            : true,
-//     version          : 'v5.0'
-// });
-// };
-// async defer src="https://connect.facebook.net/en_US/sdk.js"
-/* make the API call */
-
-
-
 function initMap(){
     var markerHolder = []; 
 
@@ -20,7 +7,42 @@ function initMap(){
 
     // The map, centered at Washington D.C.
     let map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: washingtonDC});
+        document.getElementById('map'), 
+        {zoom: 4, center: washingtonDC},
+    );
+
+    // var contentString = '<div id="content">'+
+    // '<div id="siteNotice">'+
+    // '</div>'+
+    // '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+    // '<div id="bodyContent">'+
+    // '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+    // 'sandstone rock formation in the southern part of the '+
+    // 'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+    // 'south west of the nearest large town, Alice Springs; 450&#160;km '+
+    // '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+    // 'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+    // 'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+    // 'Aboriginal people of the area. It has many springs, waterholes, '+
+    // 'rock caves and ancient paintings. Uluru is listed as a World '+
+    // 'Heritage Site.</p>'+
+    // '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+    // 'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+    // '(last visited June 22, 2009).</p>'+
+    // '</div>'+
+    // '</div>';
+
+
+    // let infoWindow = new google.maps.InfoWindow({
+    //     content: contentString
+    // });
+
+    // marker.addListener('click', function() {
+    //     infowindow.open(map, marker);
+    // });
+
+
+
     
     //Each function loops through one of the candidates (organized alphabetically) 
     //and populates map with custom marker
@@ -47,36 +69,67 @@ function initMap(){
                     continue;
                 }
 
-                let x = new google.maps.Marker(
-                    {position: 
-                        {lat: allEvents[candidate][i].lat, lng: allEvents[candidate][i].lon},
-                        map: map, 
-                        icon: {
-                            url: candidatePhotoURL,
-                        }
-                    }
-                );
+                let contentString = 
+                '<div id="content">'+
+                '<div id="eventDescription">'+
+                '</div>'+
+                `<h1 id="firstHeading" class="firstHeading">${allEvents[candidate][0]['organization']['candidate_name']}</h1>`+
+                '<div id="bodyContent">'+
+                `<p><b></b>${allEvents[candidate][i].name}<br><br>`+
+                `${allEvents[candidate][i].description}<br><br>`+
+                `Sign up: ${allEvents[candidate][i].organization.hot_leads_embed_url}<br>`+
+                '</div>'+
+                '</div>';
+            
+                let infoWindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
 
-                markerHolder.push(x)
+                let marker = new google.maps.Marker({
+                    position: {lat: allEvents[candidate][i].lat, lng: allEvents[candidate][i].lon},
+                    map: map, 
+                    icon: {
+                        url: candidatePhotoURL
+                    },
+                    // label: {
+                    //     text: 'hello world',
+                    //     color: '#222222',
+                    //     fontSize: '12px'
+                    //   }                   
+                });
+
+                marker.addListener('click', function() {
+                    infoWindow.open(map, marker);
+                });
+
+
+
+
+                markerHolder.push(marker)
+                
             }
         }
     }
+    
+
+    //calls function above to populate map with markers
     loopThruAllEvents();
-    // console.log(x)
-    // console.log(markerHolder[0])
-    // console.log(allEvents['warrenEvents'][0])
+
+    
 
 
     let toggleWarren = document.getElementById('seeWarrenEvents').onclick = function(){
-        for(var i = 0; i < markerHolder.length; i++){
-            // console.log(markerHolder[i].icon.url)
-            if(markerHolder[i].icon.url === "candidatePhotos/Warren100px.png"){
-                markerHolder[i].visible = false;
+        console.log('the Warren checkbox is working')
+
+        // for(var i = 0; i < markerHolder.length; i++){
+        //     // console.log(markerHolder[i].icon.url)
+        //     if(markerHolder[i].icon.url === "candidatePhotos/Warren100px.png"){
+        //         markerHolder[i].visible = false;
                 // location.reload();  This doesn't work as intended.
                 // markerHolder[i].hide();
                 // console.log(markerHolder[i].visible)
-            }
-        }
+        //     }
+        // }
         // console.log('the Warren checkbox is working');
         // console.log(markerHolder.length)
         // markerHolder.toggle();
@@ -85,11 +138,17 @@ function initMap(){
     let toggleYang = document.getElementById('seeYangEvents').onclick = function(){
         console.log('the Yang checkbox is working')
     }
+
+    //the below is not working yet
+    // let toggleState = document.getElementsByName('states').onclick = function(){
+    //     console.log('states')
+    // }
 }
 
 
 
 
+  
 
 
 
